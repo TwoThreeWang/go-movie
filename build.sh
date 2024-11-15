974e4c05de59e8d38654c8ebfd55af6791e39216
wangtwothree <twothreewang@gmail.com>
2024-11-15 22:32:00 +0800

    update

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ docker build -t go-movie:latest .
 echo '停止并删除旧容器'
 docker rm -f go-movie
 echo '启动容器'
-docker run --name go-movie -d -v ./config.yaml:/config.yaml -p 5002:5002 -v ./templates:/templates -v ./public:/public:rw -v ./data:/data:rw --log-opt max-size=10m --restart=always go-movie:latest
+docker run --name go-movie -d -v ./config.yaml:/config.yaml -p 8787:8787 -v ./templates:/templates -v ./public:/public:rw -v ./data:/data:rw --log-opt max-size=10m --restart=always go-movie:latest
 echo '清理不再使用的镜像、容器和数据卷'
 docker system prune --all --force --volumes
