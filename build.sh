84e52c619c14520bc9f573491704a6c3207ffeae
wangtwothree <twothreewang@gmail.com>
2025-10-18 22:13:25 +0800

    build: 使用docker-compose重构部署流程
    将原有的直接使用docker命令部署的方式改为使用docker-compose管理服务
    更新build.sh脚本以适配新的部署方式

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@ echo '开始拉取最新代码'
 git reset --hard origin/master
 git pull origin master
 echo '打包镜像'
-docker build -t go-movie:latest .
-echo '停止并删除旧容器'
-docker rm -f go-movie
+DOCKER_BUILDKIT=1 docker build -t go-movie:latest .
 echo '启动容器'
-docker run --name go-movie -d -v ./config.yaml:/config.yaml -p 8787:8787 -v ./templates:/templates -v ./public:/public:rw -v ./data:/data:rw --log-opt max-size=10m --restart=always go-movie:latest
-echo '清理不再使用的镜像、容器和数据卷'
-docker system prune --all --force --volumes
+docker-compose down
+docker-compose up --force-recreate -d --remove-orphans
